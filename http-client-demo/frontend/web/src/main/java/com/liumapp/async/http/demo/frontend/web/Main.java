6f9e0d2c198e29639df6874e70237eea630558b6
liumapp <liumapp.com@gmail.com>
2018-03-02 10:43:04 +0800

    add demo

---
+++ http-client-demo/frontend/web/src/main/java/com/liumapp/async/http/demo/frontend/web/Main.java
@@ -0,0 +1,20 @@
+package com.liumapp.async.http.demo.frontend.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by liumapp on 10/9/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@SpringBootApplication(scanBasePackages = {"com.liumapp.async.http.demo"})
+public class Main {
+
+    public static void main (String[] args) {
+
+        SpringApplication.run(Main.class , args);
+
+    }
+
+}
